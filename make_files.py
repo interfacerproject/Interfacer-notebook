f35c0069d17a7819661b4b4a8820400e19ca0524
Stefano Bocconi <Stefano.Bocconi@gmail.com>
2022-10-20 16:56:30 +0200

    Make conf files endpoint specific

---
+++ make_files.py
@@ -0,0 +1,44 @@
+import urllib.parse
+import glob
+import argparse
+from six import text_type
+import shutil
+    
+
+
+endpoint = None
+def get_filename(filename):
+    new_filename = filename.replace('_example.json',f'.{urllib.parse.quote_plus(endpoint)}.json')
+    return new_filename
+
+def copy_files():
+    files = glob.glob('*_example.json')
+    for afile in files:
+        new_file = get_filename(afile)
+        print(f'cp {afile} {new_file}')
+        shutil.copyfile(afile, new_file)
+        
+        
+    
+    
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+
+    parser.add_argument(
+        '-e', '--endpoint',
+        dest='endpoint',
+        type=text_type,
+        nargs=1,
+        default = '',
+        required=True,
+        help='specifies the endpoint of the GraphQL back-end',
+    )
+    args, unknown = parser.parse_known_args()
+
+    endpoint = args.endpoint[0]
+
+    copy_files()
